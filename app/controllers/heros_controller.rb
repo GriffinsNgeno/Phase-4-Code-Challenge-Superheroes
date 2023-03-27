7d6887df34d64de27c52d6c292151b2a267f90ae
GriffinsNgeno <117969990+GriffinsNgeno@users.noreply.github.com>
2023-03-27 20:14:56 +0300

    Update heros_controller.rb

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,2 +1,15 @@
 class HerosController < ApplicationController
+
+    def index
+        render json: Hero.all
+    end
+
+    def show
+        hero = Hero.find_by(id: params[:id])
+        if hero
+        render json: hero
+        else
+            render json: { error: "Hero not found" }, status: :not_found
+        end
+    end
 end
